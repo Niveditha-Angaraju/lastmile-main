fd0db68559270c6cd7a19248157f5b8a2bcb32d5
Niveditha-Angaraju <niveda.angaraju@gmail.com>
2025-12-09 05:38:55 +0530

    changed everything

--- services/common_lib/protos_generated/driver_pb2_grpc.py
+++ services/common_lib/protos_generated/driver_pb2_grpc.py
@@ -3,10 +3,11 @@
 import grpc
 import warnings
 
-from . import driver_pb2 as driver__pb2
+# import driver_pb2 as driver__pb2
+from services.common_lib.protos_generated import driver_pb2 as driver__pb2
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
-GRPC_GENERATED_VERSION = '1.70.0'
+GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
@@ -19,7 +20,7 @@ except ImportError:
 if _version_not_supported:
     raise RuntimeError(
         f'The grpc package installed is at version {GRPC_VERSION},'
-        + f' but the generated code in driver_pb2_grpc.py depends on'
+        + ' but the generated code in driver_pb2_grpc.py depends on'
         + f' grpcio>={GRPC_GENERATED_VERSION}.'
         + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
         + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
